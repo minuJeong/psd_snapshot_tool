c361f7d6bfb40572cc7abfe4f50bc8b421569172
Minu Jeong <minu.hanwool@gmail.com>
2017-06-29 01:28:19 +0800

    add gif/mp4 encoder

---
+++ png_to_gif_and_mp4.py
@@ -0,0 +1,15 @@
+
+import os
+
+import numpy as np
+import imageio
+from PIL import Image
+
+
+target_dir = "D:/Temp/Drawing/gafsdgdsaSfdgsbdafdsa/rvavdsafdsafds/recorder"
+target_files = [f"{target_dir}/{x}" for x in os.listdir(target_dir)]
+target_files.sort(key=lambda x: int(os.path.splitext(x)[0].split('_')[-1]))
+imgs = [Image.open(target_file) for target_file in target_files]
+arrs = [np.asarray(img) for img in imgs]
+imageio.mimwrite("dst.gif", arrs, fps=24, loop=0)
+imageio.mimwrite("dst.mp4", arrs, fps=24)
